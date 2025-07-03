d81f3fb36db7dc98c2f1f93c036453a258e6fb59
lauriekell <laurie@kell.es>
2025-07-03 07:08:36 +0100

    5th

--- R/FLBRP-rebuild.R
+++ R/FLBRP-rebuild.R
@@ -1,55 +1,6 @@
-
 #' @rdname rebuild
 #' @export
-setMethod("rebuild", signature(object="FLBRP"),
-  function(object, targetF=computeRefpts(object)["msy","harvest"]*0,
-                   targetSSB=computeRefpts(object)["msy","ssb"],
-                   nInitial=100, growthRate=0.25, minVal=1e-6, maxVal=1,
-                   burnin=20, truncate=TRUE) {
-            
-    # Input validation
-    if (!is(object, "FLBRP"))
-      stop("object must be an FLBRP object")
-            
-    if (!all(sapply(list(nInitial, burnin), function(x) is.numeric(x) && x > 0)))
-      stop("nInitial and burnin must be positive integers")
-            
-    if (!all(sapply(list(growthRate, minVal, maxVal), is.numeric)))
-      stop("growthRate, minVal, and maxVal must be numeric")
-            
-    if (minVal >= maxVal)
-      stop("minVal must be less than maxVal")
-            
-    # Generate SSB sequence
-    targetSSB=c(targetSSB) * seq(minVal^growthRate, maxVal^growthRate, 
-                                            length.out=nInitial)^(1/growthRate)
-    targetF=c(targetF)
-            
-    # Setup equilibrium
-    eql=object
-    fbar(eql)[]=0.2
-            
-    # Create target biomass array
-    btar=FLQuant(rep(targetSSB, each=dim(fbar(eql))[2]),
-                dimnames=dimnames(propagate(ssb(eql), nInitial)))
-        
-    # Project stock
-    stk=propagate(as(eql, "FLStock"), nInitial)
-    stk=fwd(stk, ssb_end=btar[,-seq(dims(stk)[["min"]]+2)], sr=eql)
-            
-    # Apply target F
-    ftar=fbar(stk) %=% targetF
-    stk=fwd(stk, f=ftar[,-seq(burnin)], sr=eql)
-            
-    # Post-process
-    if (truncate) 
-        stk=stk[,-seq(burnin)]
-            
-    stk=qapply(stk, function(x) {
-        dimnames(x)$year=seq(length(dimnames(x)$year))
-        x})
-            
-    return(stk)})
+# Duplicate setMethod for 'rebuild' with signature(object="FLBRP") removed to avoid conflicts.
 
 #' Calculate rebuilding time
 #'
--- R/biodyn-rebuild.R
+++ R/biodyn-rebuild.R
@@ -57,28 +57,31 @@ setMethod("gt", signature(x="Rebuild"),
 
 #' @rdname rebuild
 setMethod("rebuild", signature(object="numeric", p="numeric"),
-          function(object, p, k=1e3, b0=1, nyrs=50, niters=101) {
-            r <- object
-            object=biodyn(params=FLPar(r=r,p=p,k=k,B0=b0))
-            shape=c(refpts(object)["bmsy"]%/%params(object)["k"])
-            object=window(object,end=nyrs)
-            object@stock[]=refpts(object)["bmsy"]
-            object@catch[]=0
-            target=c(refpts(object)["bmsy",1])
-            object=propagate(object,niters)   
-            object@stock=object@stock%*%FLQuant(rep(seq(0,1,length.out=niters),
-                                                    each=nyrs),
-                                                dimnames=dimnames(stock(object)))
-            object=fwd(object,harvest=stock(object)[,-1]%=%0)
-            dat=cbind(target=target,as.data.frame(stock(object),drop=TRUE))
-            dat=ddply(dat,.(iter), with, {
-              rtn=try(data.frame(year=year[(data-target)^2==min((data-target)^2)][1]))
-              if ("try-error"%in%is(rtn)) return(NULL)
-              return(rtn)
-            })
-            dat=cbind(dat,initial=c(stock(object)[,1,,,,dat$iter])/target)
-            dat=dat[order(dat$initial),c("year","initial")][-1,]
-            cbind(shape=shape,dat[dat$year<nyrs,])
-          })
+  function(object, p, ...) {
+    args <- list(...)
+    k <- if (!is.null(args$k)) args$k else 1e3
+    b0 <- if (!is.null(args$b0)) args$b0 else 1
+    nyrs <- if (!is.null(args$nyrs)) args$nyrs else 50
+    niters <- if (!is.null(args$niters)) args$niters else 101
+    r <- object
+    object = biodyn(params=FLPar(r=r, p=p, k=k, B0=b0))
+    shape = c(refpts(object)["bmsy"] %/% params(object)["k"])
+    object = window(object, end=nyrs)
+    object@stock[] = refpts(object)["bmsy"]
+    object@catch[] = 0
+    target = c(refpts(object)["bmsy", 1])
+    object = propagate(object, niters)
+    object@stock = object@stock %*% FLQuant(rep(seq(0, 1, length.out=niters), each=nyrs), dimnames=dimnames(stock(object)))
+    object = fwd(object, harvest=stock(object)[, -1] %=% 0)
+    dat = cbind(target=target, as.data.frame(stock(object), drop=TRUE))
+    dat = ddply(dat, .(iter), with, {
+      rtn = try(data.frame(year=year[(data-target)^2 == min((data-target)^2)][1]))
+      if ("try-error" %in% is(rtn)) return(NULL)
+      return(rtn)
+    })
+    dat = cbind(dat, initial=c(stock(object)[,1,,,,dat$iter])/target)
+    dat = dat[order(dat$initial), c("year", "initial")][-1, ]
+    cbind(shape=shape, dat[dat$year < nyrs, ])
+  })
 
 
