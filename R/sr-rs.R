2c2bec7ff6a815370b155011ea7e767188e94802
lauriekell <laurie@kell.es>
2025-07-16 20:28:17 +0100

    rmax based on ssb

--- R/sr-rs.R
+++ R/sr-rs.R
@@ -68,15 +68,14 @@ setMethod("invSRR", signature(params = "FLSR", rec = "FLQuant"),
 #' @rdname refCreate
 #' @export
 setMethod("refCreate", signature(object = "FLBRP"), function(object, ref, value = NA, ...) {
-  rtn = list(refpt = ref,
+  rtn = list(refpt=ref,value=ref,
              quant = c("harvest", "yield", "rec",
                        "ssb", "biomass", "revenue",
                        "cost", "profit"),
              iter = ifelse(is.na(value), 1, seq(value)))
   rtn = FLPar(NA, dimnames = rtn)
-  rtn[ref, "rec"] = value
-  rtn
-})
+  rtn[ref, value] = value
+  rtn})
 
 #' @rdname rmax
 #' @export
@@ -88,16 +87,19 @@ setMethod("rmax", signature(object="FLBRP",ratio="missing"),
          segreg  = params(object)["a"] %/% params(object)["a"])})
 
 setMethod("rmax", signature(object="FLBRP",ratio="numeric"), 
-          function(object,ratio) {
-            refpts(object) = refCreate(object, "rmax", rmax(object)*ratio)
-            computeRefpts(object)})
+     function(object,ratio) {
+        #invSRR
+        #refpts(object)=refCreate(object, "rmax", "rec", rmax(object)*ratio)
+        refpts(object)=refCreate(object, "rmax","ssb", srrInv(object,rmax(object)*ratio))
+        computeRefpts(object)})
 
 #' @rdname rmsy
 #' @export
 setMethod("rmsy", signature(object="FLBRP"), 
-          function(object,ratio=1.0) {
-  refpts(object) = refCreate(object, "rmsy", refpts(object)["msy","rec"]*ratio)
-  computeRefpts(object)})
+      function(object,ratio=1.0) {
+        #refpts(object)=refCreate(object, "rmsy","rec", refpts(object)["msy","rec"]*ratio)
+        refpts(object)=refCreate(object, "rmsy","ssb", srrInv(object,refpts(object)["msy","rec"]*ratio))
+        computeRefpts(object)})
 
 # =============================================================================
 # Helper Functions (not S4)
